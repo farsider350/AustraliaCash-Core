0ef803b6575894b76dcc3815beaf173b7815bc65
farsider350 <farsider350@github.com>
2023-04-05 23:45:29 +0000

    Max Re-org

--- src/init.cpp
+++ src/init.cpp
@@ -1046,7 +1046,7 @@ bool AppInitParameterInteraction(const ArgsManager& args, bool use_syscall_sandb
     }
 #endif // USE_SYSCALL_SANDBOX
 
-    nMaxReorgLength = GetArg("-maxreorglength", DEFAULT_MAX_REORG_LENGTH);
+    nMaxReorgLength = gArgs.GetArg("-maxreorglength", DEFAULT_MAX_REORG_LENGTH);
 
     return true;
 }
